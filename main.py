513812a6dfd1d89dc1279d19ebaa8d0950b79366
Kseniyapal <100351525+Kseniyapal@users.noreply.github.com>
2022-05-01 11:03:29 +0300

    Задание

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+z = pd.read_csv("zillow.csv")
+print(z)
+zl=z.groupby(["Beds"])
+plt.figure(figsize=(50, 20))
+fig, ax = plt.subplots(nrows=1,ncols=len(zl.groups.keys()))
+for (key,ax) in zip(zl.groups.keys(),ax.flatten()):
+    zl.get_group(key).plot(ax=ax)
+    ax.set_title(key)
+    ax.legend()
 
+plt.show()
